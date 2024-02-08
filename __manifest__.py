bde41d5f2266bee5bc309d033d338756323660e6
unknown <hamemiiheb11@gmail.com>
2024-02-08 15:56:56 +0100

    first commi

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+{
+    
+
+'name': 'Hospital Management', 
+'version': '1.0.0',
+'category': 'hospital', 
+'sequence':'-100',
+'summary': 'hospital management system',
+'depends': [],
+'data': [
+    'views/menu.xml',
+    'views/patient_view.xml',
+],
+'demo': [],
+'application':True,
+'auto_install': False,
+'license':'AGPL-3',
+
+}
